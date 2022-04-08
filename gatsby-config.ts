d88cc82dd67d4194c5ba0f086372b768c90a0d33
jupemara <jumpei.arashi@arashike.com>
2022-04-09 07:49:19 +0900

    tentatively use default dir src/pages

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,11 +1,26 @@
-import type { GatsbyConfig } from "gatsby"
+import path from 'path';
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `blog`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        extensions: ['.mdx', 'md'],
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: path.resolve('src/pages'),
+      },
+    },
+  ],
+};
 
-export default config
+export default config;
