52555ac066dc010ba63acae690968468b312f8c5
jupemara <jumpei.arashi@arashike.com>
2022-08-03 15:54:59 +0900

    code highlighting with prismjs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import 'the-new-css-reset/css/reset.css';
+import 'prism-themes/themes/prism-vsc-dark-plus.min.css';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
