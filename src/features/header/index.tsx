d8dbbb5b4e391d73f9ee2b6cc5992ff89865eaaa
jupemara <jumpei.arashi@arashike.com>
2022-08-04 23:46:08 +0900

    PC space-tunnel

--- src/features/header/index.tsx
+++ src/features/header/index.tsx
@@ -7,15 +7,16 @@ export const Header: FC = (props) => {
   return (
     <>
       <div className={styles['title-outer']}>
-        <div className={styles.title}>
+        <span className={styles['space-tunnel']}></span>
+        <span className={styles.title}>
           <BorderPlaneWithMother2Font>
             <Link href="/">
               <a>blog.arashike.com</a>
             </Link>
           </BorderPlaneWithMother2Font>
-        </div>
-        <div className={styles.header}></div>
+        </span>
       </div>
+      <div className={styles.header}></div>
     </>
   );
 };
