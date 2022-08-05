43e66b0ce037519e70b37eb11242fa893f78cad6
jupemara <jumpei.arashi@arashike.com>
2022-08-05 18:27:10 +0900

    PC: space-tunnel + warp

--- src/features/header/index.tsx
+++ src/features/header/index.tsx
@@ -6,17 +6,20 @@ import styles from './styles/index.module.css';
 export const Header: FC = (props) => {
   return (
     <>
-      <div className={styles['title-outer']}>
-        <span className={styles['space-tunnel']}></span>
-        <span className={styles.title}>
-          <BorderPlaneWithMother2Font>
-            <Link href="/">
-              <a>blog.arashike.com</a>
-            </Link>
-          </BorderPlaneWithMother2Font>
-        </span>
-      </div>
-      <div className={styles.header}></div>
+      <header>
+        <div className={styles['title-outer']}>
+          <span className={styles['space-tunnel']}></span>
+          <span className={styles.title}>
+            <BorderPlaneWithMother2Font>
+              <Link href="/">
+                <a>blog.arashike.com</a>
+              </Link>
+            </BorderPlaneWithMother2Font>
+          </span>
+          <a href="https://example.com" className={styles.warp}></a>
+        </div>
+        <div className={styles['header-bottom']}></div>
+      </header>
     </>
   );
 };
