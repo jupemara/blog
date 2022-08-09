d15ce48cc0e92bcebede8deb2b7155eae837b249
jupemara <jumpei.arashi@arashike.com>
2022-08-10 00:52:08 +0900

    rename タコ・ソ・ノモノ

--- src/components/Octobot/index.tsx
+++ src/components/Octobot/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export const Octobot = (): JSX.Element => {
   return (
     <Image
-      alt="octobot: マル・デ・タコ"
+      alt="octobot: タコ・ソ・ノモノ"
       src="/images/components/octobot.gif"
       width={32}
       height={48}
