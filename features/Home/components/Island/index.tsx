d097093692b09844d022ff8e960612ecfa17db22
jupemara <jumpei.arashi@arashike.com>
2022-08-11 03:04:37 +0900

    tentatively randomly appear warp-object

--- features/Home/components/Island/index.tsx
+++ features/Home/components/Island/index.tsx
@@ -1,34 +1,53 @@
-import Image from 'next/image';
 import styles from './styles/index.module.css';
 
 type IslandProps = {
   title: string;
   lastUpdatedAt: string;
   thumbnail: string;
+  top: number;
+  left: number;
 };
 
 // TODO: define props
-export const Island = ({ title, lastUpdatedAt, thumbnail }: IslandProps) => {
+export const Island = ({
+  title,
+  lastUpdatedAt,
+  thumbnail,
+  top,
+  left,
+}: IslandProps) => {
+  const offset = 80;
   return (
     <div className={styles.island}>
-      <div className={styles.header}></div>
       <div className={styles.main}>
-        <h2>
-          <Image
-            alt={title}
-            src="/images/components/warp.png"
-            width={64}
-            height={96}
-          ></Image>
-          {title}
-        </h2>
-        <div className={styles['published-date']}>{lastUpdatedAt}</div>
-        <Image
-          alt={title}
-          src="/test-002/trrs-test.jpg"
-          width={128}
-          height={128}
-        ></Image>
+        <div className={styles['title-container']}>
+          <div className={styles['warp-container']}>
+            <div
+              className={`${styles.warp} ${styles['warp-ball']}`}
+              style={{
+                top: `${top}px`,
+                left: `${left}px`,
+              }}
+            ></div>
+            <div
+              className={`${styles.warp} ${styles['warp-shadow']}`}
+              style={{
+                top: `${top + offset}px`,
+                left: `${left}px`,
+              }}
+            ></div>
+            <h2 className={styles.title}>{title}</h2>
+            <p className={styles.short}>
+              コレはショートテキストでテスト的に書いています.
+              時々キーボードデザイナーとして仕事をしたいなと思いつつ、フロントエンドとバックエンドでソフトウェアエンジニアをやっています.
+              とはいえ, 日々の生活はそれなりに楽しく,
+              明日もポリカーボネートのトッププレートが届くので最高に楽しみです.
+            </p>
+          </div>
+        </div>
+        <div className={styles['published-date-container']}>
+          <div className={styles['published-date']}>{lastUpdatedAt}</div>
+        </div>
       </div>
       <div className={styles.footer}></div>
     </div>
