2f202d68f93f061b55e18a25ce65edeadf5a96ef
jupemara <jumpei.arashi@arashike.com>
2022-08-10 16:26:38 +0900

    island frame with grid

--- features/Home/components/Island/index.tsx
+++ features/Home/components/Island/index.tsx
@@ -10,23 +10,27 @@ type IslandProps = {
 // TODO: define props
 export const Island = ({ title, lastUpdatedAt, thumbnail }: IslandProps) => {
   return (
-    <div>
-      <h2>
+    <div className={styles.island}>
+      <div className={styles.header}></div>
+      <div className={styles.main}>
+        <h2>
+          <Image
+            alt={title}
+            src="/images/components/warp.png"
+            width={64}
+            height={96}
+          ></Image>
+          {title}
+        </h2>
+        <div className={styles['published-date']}>{lastUpdatedAt}</div>
         <Image
           alt={title}
-          src="/images/components/warp.png"
-          width={64}
-          height={96}
+          src="/test-002/trrs-test.jpg"
+          width={128}
+          height={128}
         ></Image>
-        {title}
-      </h2>
-      <div className={styles['published-date']}>{lastUpdatedAt}</div>
-      <Image
-        alt={title}
-        src="/test-002/trrs-test.jpg"
-        width={128}
-        height={128}
-      ></Image>
+      </div>
+      <div className={styles.footer}></div>
     </div>
   );
 };
