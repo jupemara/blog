2bc467f772cc0524fe493c47a3f12911e93adf0a
jupemara <jumpei.arashi@arashike.com>
2022-08-15 12:41:05 +0900

    datetime format validation

--- lib/posts.ts
+++ lib/posts.ts
@@ -6,6 +6,7 @@ import { unified } from 'unified';
 import remarkParse from 'remark-parse';
 import remarkStringify from 'remark-stringify';
 import strip from 'strip-markdown';
+import dayjs from 'dayjs';
 
 const Meta = z.object({
   slug: z.string(),
@@ -36,7 +37,14 @@ export function getAllPosts(): Post[] {
     .keys()
     .filter((v: string) => v.endsWith('.md') || v.endsWith('.mdx'))
     .map((v: string) => {
-      const { data, content } = matter(ctx(v).default);
+      const { data, content } = matter(ctx(v).default),
+        publishedAt = dayjs(data.publishedAt),
+        updatedAt = dayjs(data.updatedAt);
+      if (!publishedAt.isValid() || !updatedAt.isValid()) {
+        throw new Error(
+          `"publishedAt" or "updatedAt" datetime format is invalud. p: "${data.publishedAt}", u: "${data.updatedAt}"`,
+        );
+      }
       const txt = unified()
         .use(remarkParse)
         .use(remarkStringify)
