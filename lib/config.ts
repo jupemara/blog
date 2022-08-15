0791f4a7a983bc0765b56610179f98988c11cbac
jupemara <jumpei.arashi@arashike.com>
2022-08-15 20:22:08 +0900

    gtag.js depends on environment vars

--- lib/config.ts
+++ lib/config.ts
@@ -2,4 +2,5 @@ export const config = {
   host: 'blog.arashike.com',
   site: 'blog.arashike.com',
   author: 'jupemara',
+  gaMeasurementId: process.env.GA_ID || '',
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,19 +3,23 @@ import 'highlight.js/styles/tokyo-night-dark.css';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Script from 'next/script';
+import { config } from 'lib/config';
 
 function App({ Component, pageProps }: AppProps) {
+  if (!config.gaMeasurementId) {
+    return <></>;
+  }
   return (
     <>
       <Script
         async
-        src="https://www.googletagmanager.com/gtag/js?id=G-JHJW8RB2CH"
+        src={`https://www.googletagmanager.com/gtag/js?id=${config.gaMeasurementId}`}
       ></Script>
       <Script id="ga">
         {`window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'G-JHJW8RB2CH');`}
+gtag('config', '${config.gaMeasurementId}');`}
       </Script>
       <Component {...pageProps} />
     </>
