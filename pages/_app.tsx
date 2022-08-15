8d806b435c72b50ac1daabf1cfa3eb0f735d43eb
jupemara <jumpei.arashi@arashike.com>
2022-08-15 19:04:02 +0900

    install google analytics with gtag.js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,24 @@ import 'modern-css-reset/dist/reset.min.css';
 import 'highlight.js/styles/tokyo-night-dark.css';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-JHJW8RB2CH"
+      ></Script>
+      <Script>
+        {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-JHJW8RB2CH');`}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp;
+export default App;
