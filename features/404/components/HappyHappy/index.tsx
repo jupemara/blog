e97c3cf8d180d52f61f13945c1b1d39421b224cb
jupemara <jumpei.arashi@arashike.com>
2022-08-16 12:06:21 +0900

    404 body background

---
+++ features/404/components/HappyHappy/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles/index.module.css';
+
+export const HappyHappy = (): JSX.Element => {
+  return (
+    <main>
+      <div className={styles.container}></div>
+    </main>
+  );
+};
