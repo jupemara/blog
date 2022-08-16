9e1a41e06703da76342961fc4b47daf62cb25ce0
jupemara <jumpei.arashi@arashike.com>
2022-08-16 19:17:20 +0900

    randomize 600, 700, 800ms cultist

--- features/404/components/Devotee/index.tsx
+++ features/404/components/Devotee/index.tsx
@@ -1,5 +1,15 @@
+import { random } from 'lib/utils/random-range';
+import { useEffect, useState } from 'react';
 import styles from './styles/index.module.css';
 
 export const Devotee = (): JSX.Element => {
-  return <div className={styles.devotee}></div>;
+  const [devotee, setDevotee] = useState('');
+  useEffect(() => {
+    setDevotee(
+      [styles['devotee-600'], styles['devotee-700'], styles['devotee-800']][
+        random(0, 2)
+      ],
+    );
+  });
+  return <div className={devotee}></div>;
 };
